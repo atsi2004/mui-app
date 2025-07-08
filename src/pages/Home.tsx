546c2898af11ea96855ad753bd5a22a3917c3b89
atsi2004 <60413782+atsi2004@users.noreply.github.com>
2025-07-08 11:51:34 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,10 @@ import { Box, Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import '../i18n'; 
 import {useTranslation } from 'react-i18next';
-import LanguageSelector from '../components/LanguageSelector';
-import ThemeToggle from '../components/ThemeToggle'; 
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <Box textAlign="center" mt={10}>
